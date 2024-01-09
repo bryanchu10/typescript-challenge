958eceada9f94578ba7e46d3ce473e8f4cb8e5ee
bryan <bryanchu10@gmail.com>
2024-01-09 23:28:34 +0800

    fix: objectAccessor

--- src/objectAccessor.ts
+++ src/objectAccessor.ts
@@ -12,6 +12,13 @@
  * @param obj - 一個物件
  * @returns - 回傳一個物件，該物件有 get 和 set 兩個方法
  */
-export function createObjectAccessor<T>(obj: T) {
-    // 請在此處寫下你的程式碼
+const createObjectAccessor = <T>(obj: T) => {
+    return {
+        get: <K extends keyof T>(key: K): T[K] => obj[key],
+        set: <K extends keyof T>(key: K, value: T[K]) => obj[key] = value,
+    }
+}
+
+export {
+    createObjectAccessor,
 }
